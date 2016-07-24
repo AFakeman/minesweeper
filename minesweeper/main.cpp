b471ab2d3bf32e6164764ec756d2dba4fc1a5543
AFakeman <anton_suslov@me.com>
2016-07-24 15:41:41 +0300

    Initial Commit

---
+++ minesweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  minesweeper
+//
+//  Created by Anton Suslov on 24/07/16.
+//  Copyright © 2016 Anton Suslov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
